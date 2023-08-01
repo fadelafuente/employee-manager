f3f7bd7078633c82355a72393b62e8afdd34a81f
fadelafuente <f.delafuente@outlook.com>
2023-08-01 15:24:52 -0500

    add exception handling

--- src/main/java/tech/getarrays/employee/manager/Employee/EmployeesService.java
+++ src/main/java/tech/getarrays/employee/manager/Employee/EmployeesService.java
@@ -1,8 +1,12 @@
 package tech.getarrays.employee.manager.Employee;
 
+import ch.qos.logback.core.model.conditional.ElseModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.getarrays.employee.manager.exception.UserNotFoundException;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -17,5 +21,21 @@ public class EmployeesService {
     public Employee addEmployee(Employee employee) {
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return repository.save(employee);
+    }
+
+    public List<Employee> findEmployees() {
+        return repository.findAll();
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new UserNotFoundException("User with id " + id + " was not found"));
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return repository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        repository.deleteById(id);
     }
 }
